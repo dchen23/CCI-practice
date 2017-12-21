701f31c27c3fc82478562169c48a650ac7b8c5d4
dchen23 <dchen23@usfca.edu>
2017-12-20 23:27:54 -0800

    first blood

--- java/ch1/ch01_q01/QuestionA.java
+++ java/ch1/ch01_q01/QuestionA.java
@@ -2,4 +2,22 @@ package ch01_q01;
 
 public class QuestionA {
 
+    static boolean isUnique(String strs) {
+        boolean[] char_set = new boolean[128];
+        for (int i = 0; i < strs.length(); i++) {
+            int val = strs.charAt(i);
+            System.out.println(val);
+            if (char_set[val])
+                return false;
+            char_set[val] = true;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String[] words = {"abcde", "hello", "apple", "kite", "padle"};
+        for (String word : words) {
+            System.out.println(word + ": " + isUnique(word));
+        }
+    }
 }
